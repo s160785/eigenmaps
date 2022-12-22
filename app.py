41ec3761f5d9e7488a45701d5b84eee98e7b2430
S.T.Santhosh Kumar <69844462+s160785@users.noreply.github.com>
2022-12-23 00:50:29 +0530

    Update app.py
    added api for yolo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 import socket       
+import os
 
 @app.route("/")
 def home():
@@ -12,4 +13,35 @@ def getip():
     hostname=socket.gethostname()     #Desktop Name
     IP_ADDRESS =socket.gethostbyname(hostname) #  IP Adress
     print("IP Address is:"+IP_ADDRESS) 
-    return f"MY ip address is {IP_ADDRESS}"
+    return f"MY ip address is {IP_ADDRESS}""
+
+@app.route('/upload')
+def upload_file():
+   return """<html>
+                <body>
+                    <form action = "http://localhost:5000/object_count" method = "POST" 
+                        enctype = "multipart/form-data">
+                        <input type = "file" name = "file" />
+                        <input type = "submit"/>
+                    </form>
+                </body>
+            </html>"""
+
+@app.route("/object_count",methods=["POST"])
+def find_obj_count():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(f.filename)
+        import subprocess
+        os.chdir("./yolov4-deepsort")
+        print(os.getcwd())
+        cmd = f'python object_tracker.py --weights ./checkpoints/yolov4-tiny-416 --model yolov4 --video ../{f.filename} --output ../{f.filename.split(".")[0]}_tiny.avi --tiny --count'
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
+        out, err = p.communicate() 
+        result = str(out).split('\n')
+        return f"""<html><body>
+                <h1> total no of objects in image is {result[-1].strip().split()[-1][:-5]} <h2>
+                </body>
+                </html>
+                """
